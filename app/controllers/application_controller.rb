a5a2bed8922c3e660ada591b5d632d253bbb063e
Marlos <marlos.irapuan@gmail.com>
2025-04-26 14:12:22 -0300

    feat: add validate token in request application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::API
+  before_action :authenticate_request
+
+  private
+
+  def authenticate_request
+    header  = request.headers["Authorization"]
+    header  = header.split(" ").last if header
+    decoded = JsonWebToken.decode(header)
+
+    @current_user = User.find_by(id: decoded[:user_id]) if decoded
+
+    render json: { error: "Unauthorized" }, status: :unauthorized unless @current_user
+  end
 end
