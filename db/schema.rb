95830e6c1e7894969e01ea5301d8f42a7609b7c0
Marlos <marlos.irapuan@gmail.com>
2025-04-26 13:01:59 -0300

    feat: add migration, models and tests to document

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_26_155130) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_26_155848) do
+  create_table "documents", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "file_path"
+    t.string "signature_path"
+    t.boolean "signed"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_documents_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -18,4 +28,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_26_155130) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "documents", "users"
 end
