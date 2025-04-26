84f2eb3a67232181eb978b9e15b37d310d44dcd3
Marlos <marlos.irapuan@gmail.com>
2025-04-26 15:35:17 -0300

    test: add assertions to versions

--- test/models/document_test.rb
+++ test/models/document_test.rb
@@ -17,4 +17,33 @@ class DocumentTest < ActiveSupport::TestCase
     assert_not document.valid?
     assert_includes document.errors[:file_path], "can't be blank"
   end
+
+  test "should create a version when document is updated" do
+    # Arrange
+    document = Document.create!(
+      user: users(:one),
+      file_path: "old_file_path.pdf",
+      signature_path: "signature_path.png",
+      signed: true
+    )
+    original_file_path = document.file_path
+
+    # Act
+    document.update!(file_path: "new_file_path.pdf")
+
+    # Assert
+    assert_equal 2, document.versions.size
+    assert_equal original_file_path, document.versions.last.reify.file_path
+  end
+
+  test "should not create a version when document is not changed" do
+    # Arrange
+    document = documents(:one)
+
+    # Act
+    document.touch
+
+    # Assert
+    assert_equal 1, document.versions.size
+  end
 end
