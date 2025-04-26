224337a808b0242ec919a21d2b328e5b54637374
Marlos <marlos.irapuan@gmail.com>
2025-04-26 14:11:25 -0300

    feat: add json web token with tests

---
+++ lib/json_web_token.rb
@@ -0,0 +1,15 @@
+module JsonWebToken
+  SECRET_KEY = Rails.application.credentials.secret_key_base
+
+  def self.encode(payload, exp = 12.hours.from_now)
+    payload[:exp] = exp.to_i
+    JWT.encode(payload, SECRET_KEY)
+  end
+
+  def self.decode(token)
+    body = JWT.decode(token, SECRET_KEY)[0]
+    HashWithIndifferentAccess.new body
+  rescue
+    nil
+  end
+end
