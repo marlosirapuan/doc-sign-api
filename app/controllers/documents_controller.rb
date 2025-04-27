5b9b9207de9dc01841bf9b04485ccce67a531a5c
Marlos <marlos.irapuan@gmail.com>
2025-04-27 08:46:31 -0300

    feat: add destroy action

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,6 +1,8 @@
 class SignatureNotFoundError < StandardError; end
 
 class DocumentsController < ApplicationController
+  before_action :set_document, only: [ :download, :destroy ]
+
   rescue_from SignatureNotFoundError, with: :handle_signature_error
 
   def index
@@ -15,11 +17,18 @@ class DocumentsController < ApplicationController
     render json: { document: document }, status: :created
   end
 
-  def download
-    document = @current_user.documents.find(params[:id])
+  def destroy
+    delete_file(@document.file_path)
+    delete_file(@document.signature_path)
 
-    if File.exist?(document.file_path)
-      send_file document.file_path, type: "application/pdf", disposition: "inline"
+    @document.destroy
+
+    render json: { message: "Document successfully deleted." }, status: :ok
+  end
+
+  def download
+    if File.exist?(@document.file_path)
+      send_file @document.file_path, type: "application/pdf", disposition: "inline"
     else
       render json: { error: "File not found" }, status: :not_found
     end
@@ -27,5 +36,15 @@ class DocumentsController < ApplicationController
 
   def handle_signature_error(error)
     render json: { error: error.message }, status: :unprocessable_entity
+  end
+
+  private
+
+  def set_document
+    @document = @current_user.documents.find(params[:id])
+  end
+
+  def delete_file(path)
+    File.delete(path) if path.present? && File.exist?(path)
   end
 end
